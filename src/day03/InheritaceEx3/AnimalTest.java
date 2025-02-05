8bf666e0cfb442599a0d2e416ecb0e7e6699b057
seonmin12 <seonmink12@naver.com>
2025-02-05 13:58:22 +0900

    test

---
+++ src/day03/InheritaceEx3/AnimalTest.java
@@ -0,0 +1,19 @@
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		Animal dog = new Dog(8);
+		Animal chicken = new Chicken(3);
+		Chicken cheatableChicken = new Chicken(5);
+	
+	}
+
+
+
+
+
+
+
+
+
+
